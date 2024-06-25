ad8af8d6b41d16b047cb8e9bbed0ef1f0a52da4c
Thibaut Rousseau <thibaut.rousseau.44@gmail.com>
2024-06-25 22:13:23 +0200

    increase test coverage

--- tests/stdlib/transactor_test.go
+++ tests/stdlib/transactor_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/Thiht/transactor/stdlib"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -57,6 +58,28 @@ func TestTransactor(t *testing.T) {
 		require.NoError(t, mock.ExpectationsWereMet())
 	})
 
+	t.Run("it should return an error if the commit fails", func(t *testing.T) {
+		t.Parallel()
+
+		db, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			db.Close()
+		})
+
+		transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsNone)
+
+		mock.ExpectBegin()
+		mock.ExpectCommit().WillReturnError(assert.AnError)
+
+		err = transactor.WithinTransaction(context.Background(), func(_ context.Context) error {
+			return nil
+		})
+		require.Error(t, err)
+
+		require.NoError(t, mock.ExpectationsWereMet())
+	})
+
 	t.Run("with no nested transactions support", func(t *testing.T) {
 		t.Parallel()
 
@@ -91,7 +114,7 @@ func TestTransactor(t *testing.T) {
 	t.Run("with nested transactions savepoints", func(t *testing.T) {
 		t.Parallel()
 
-		t.Run("it should rollback the nested transaction", func(t *testing.T) {
+		t.Run("it should rollback the nested transaction in case of error", func(t *testing.T) {
 			t.Parallel()
 
 			db, mock, err := sqlmock.New()
@@ -110,6 +133,93 @@ func TestTransactor(t *testing.T) {
 			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
 				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
 					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return the original error in case of failure to rollback a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsSavepoints)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectExec("ROLLBACK TO SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+				require.ErrorContains(t, err, "an error occurred")
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return an error in case of failure to begin a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsSavepoints)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return nil
+				})
+				require.Error(t, err)
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return an error in case of failure to commit a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsSavepoints)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectExec("RELEASE SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return nil
 				})
 				require.Error(t, err)
 
@@ -204,6 +314,188 @@ func TestTransactor(t *testing.T) {
 					require.NoError(t, err)
 
 					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+	})
+
+	t.Run("with nested transactions mssql", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("it should rollback the nested transaction in case of error", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsMSSQL)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVE TRANSACTION sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectExec("ROLLBACK TRANSACTION sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return the original error in case of failure to rollback a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsMSSQL)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVE TRANSACTION sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectExec("ROLLBACK TRANSACTION sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+				require.ErrorContains(t, err, "an error occurred")
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return an error in case of failure to begin a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsMSSQL)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVE TRANSACTION sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return nil
+				})
+				require.Error(t, err)
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+	})
+
+	t.Run("with nested transactions oracle", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("it should rollback the nested transaction in case of error", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsOracle)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectExec("ROLLBACK TO SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return the original error in case of failure to rollback a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsOracle)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectExec("ROLLBACK TO SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return errors.New("an error occurred")
+				})
+				require.Error(t, err)
+				require.ErrorContains(t, err, "an error occurred")
+
+				return nil
+			})
+			require.NoError(t, err)
+
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+
+		t.Run("it should return an error in case of failure to begin a nested transaction", func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				db.Close()
+			})
+
+			transactor, _ := stdlib.NewTransactor(db, stdlib.NestedTransactionsOracle)
+
+			mock.ExpectBegin()
+			mock.ExpectExec("SAVEPOINT sp_1").WillReturnResult(sqlmock.NewResult(0, 0)).WillReturnError(assert.AnError)
+			mock.ExpectCommit()
+
+			err = transactor.WithinTransaction(context.Background(), func(ctx context.Context) error {
+				err := transactor.WithinTransaction(ctx, func(_ context.Context) error {
+					return nil
 				})
 				require.Error(t, err)
 
